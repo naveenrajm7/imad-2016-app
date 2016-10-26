b05e2840d14647140b9a76e09f4a232050435795
naveenrajm7 <naveenrajm97@gmail.com>
2016-10-26 23:40:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,26 @@
-console.log('Loaded!');
-//change the text of the main-text div
-var element =document.getElementById('main-text');
-
-element.innerHTML='New Value';
-
-//move the image
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft+'px';
-}    
-
-    img.onclick=function(){
-        var interval=setInterval(moveRight,50);
+//counter code
+var button =document.getElementById('counter');
+    var counter=0;
+    
+button.onclick=function(){
+    //creat a request object
+    var request= new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            //take some action
+            if(request.status===200){
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+                
+            }
+        }
+        //not done yet
     };
+    
+    //make a request
+    request.open('GET','http://naveenrajm7.imad.hasura-app.io/counter',true);
+    request.send(null);
+};    

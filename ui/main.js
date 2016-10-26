1ea81546ebbf2bda38e206f2278ddd308c647154
naveenrajm7 <naveenrajm97@gmail.com>
2016-10-27 00:05:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,4 +23,20 @@ button.onclick=function(){
     //make a request
     request.open('GET','http://naveenrajm7.imad.hasura-app.io/counter',true);
     request.send(null);
-};    
+}; 
+//submit name
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+    //make request to the server and send the name
+    
+    //capture the list names and render it as a list
+    var nmaes=['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+='<li>' +names[i]+'<li>';
+    }
+    var ul=document.getElementById('nmaelist');
+    ul.innerHTML=list;
+};
